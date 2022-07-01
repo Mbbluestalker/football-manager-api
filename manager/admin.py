819ffaaeab18677985caae0943722ac2281d1577
Akorede Olawole <akoredeolawole@gmail.com>
2022-07-01 21:54:44 +0100

    added all models to admin.py

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Player, Team, User
+
+admin.site.register(User)
+admin.site.register(Team)
+admin.site.register(Player)
